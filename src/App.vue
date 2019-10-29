2de45767baacb588f8cbc24ca9eaffd4e5431a7c
negimohit1209 <mohitsngh1997@gmail.com>
2019-10-29 15:37:45 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+    export default {
+        data: function(){
+            return {
+                maxQuotes: 10,
+                quotes: [
+                    'Just a quote to see something.'
+                ],
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
+    }
+</script>
+
+<style>
+</style>
